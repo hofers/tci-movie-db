642d822384a01f18cb4b1913d8c27aef9b535bae
Sean Hofer <Sean@Laurens-MacBook-Pro-2.local>
2019-07-24 10:05:41 -0700

    updated ruby version for heroku publishing

--- movie-db/db/schema.rb
+++ movie-db/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_10_200849) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reviews", force: :cascade do |t|
     t.integer "movie_id"
     t.string "email"
